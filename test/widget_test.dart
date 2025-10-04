4a9bacf051b517a5fead786c657e1eef36d2601e
Darshilkumar Karkar <darshilkarkar012@gmail.com>
2025-10-04 02:27:24 -0400

    update

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,14 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mad201_assignment01_darshilkumar_karakr/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('App loads smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that the app loads and contains the HomeScreen widget.
+    expect(find.byType(MaterialApp), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Optional: check that a search field exists on home screen.
+    expect(find.byType(TextField), findsOneWidget);
   });
 }
